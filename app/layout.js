19bd527da808b18d0da892adde66605ea4ae4bde
= <michaelviloria.g@gmail.com>
2023-09-10 11:58:11 -0500

    restableciendo

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
 import "./globals.css";
-import SessionAuthProvider from "@/components/SessionAuthProvider";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
         <title>Washing Club</title>
       </head>
       <body className="flex flex-col justify-center w-full max-w-lg min-h-screen p-4 mx-auto bg-white rounded-md shadow-lg ">
-        <SessionAuthProvider>{children}</SessionAuthProvider>
+        {children}
       </body>
     </html>
   );
